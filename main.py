8a064150ba33937289ca054768726e205ab13880
M-y-M <magnello.marcos@gmail.com>
2026-06-16 15:34:50 -0300

    Desarrollo del Esqueleto del Programa
    Se agregaron los módulos auxiliares con las funciones a desarrollar en cada uno de ellos

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#from archivos import inicializar_csv
+#from solicitudes import generar_solicitud, modificar_tipo_licencia, modificar_fecha_tentativa
+
+
+
+
+
 # MAQUINA DE ESTADOS DEL PROGRAMA
 estado_usuario = {
     "etapa": "inicio",  # inicio / menu / fechas / confirmacion
@@ -6,7 +13,8 @@ estado_usuario = {
     "fecha_inicio": None,
     "dias_solicitados": None,
 }
-
+def salida(texto):
+    return texto.strip().lower() in ["salir", "cancelar", "exit", "4"]
 # POSIBLE FUNCION PARA CORTAR EL FLUJO Y SALIR. MODIFICAR
 
 
@@ -18,3 +26,52 @@ def procesar_mensaje(texto, estado):
     # recién acá procesás el flujo normal según la etapa
     if estado["etapa"] == "menu":
         ...
+
+# BLOQUE PRINCIPAL
+def main():
+    while True:  # aca poner el menu con sus validaciones
+        print("\n" + "=" * 50)
+        print("SISTEMA DE GESTIÓN DE LICENCIAS".center(50))
+        print("=" * 50)
+
+        print(
+            "\nBienvenido al ChatBot de solicitudes de livencia."
+            "\nEscriba 'salir en cualquier momento para cancelar.\n"
+        )
+        while True:  # validacion de errores en las opciones del menu
+            try:
+                print("\n=============== MENÚ PRINCIPAL ===============")
+                print("\n¿Que desea hacer?"
+                    "\n1. Genera nueva solicitud"
+                    "\n2. Modificar tipo de licencia"
+                    "\n3. BModificar f3echa tentativa"
+                    "\n4. Salir.\n"
+                )
+                print("==============================================\n")
+                menu = int(input("Su eleccion: ")).strip()
+                print()
+                if menu not in range(1, 5):
+                    print(
+                        "No has ingresado un numero valido. Ingresa una opcion entre 1 y 7\n"
+                    )
+                else:
+                    break
+            except ValueError:
+                print(
+                    "No has ingresado una opcion valida. Ingresa solamente un numero entre 1 y 7.\n"
+                )
+
+        # estructura match- case del menu principal
+        match menu:
+            case 1:
+                ...
+
+            case 2:
+                ...
+
+            case 3:
+                ...
+
+
+if __name__ == "__main__":
+    main()
