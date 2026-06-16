b3eb9c5e66c39801889b0b7e86106c356f8523fa
M-y-M <magnello.marcos@gmail.com>
2026-06-16 17:28:00 -0300

    Definicion de la función buscar_emplado(dni):
    Defino la función buscar_empleado, que es utilizada para validar si el dni ingresado en el input que se pondrá en el main como requisito inicial esta o no en el registro.

---
+++ solicitudes.py
@@ -0,0 +1,16 @@
+
+#Flujo 1: Generar solicitud
+def generar_solicitud():
+    pass
+
+
+
+#Flujo 2: Modificar tipo de licencia
+def modificar_tipo_licencia():
+    pass
+
+
+
+#Flujo 3: Modificar fecha tentativa
+def modificar_fecha_tentativa():
+    pass
---
+++ validaciones.py
@@ -0,0 +1,15 @@
+
+
+def validar_dni():
+    pass
+
+def validar_fecha():
+    pass
+
+def validar_dias():
+    pass
+
+def tipo_descripcion():
+    pass
+
+
