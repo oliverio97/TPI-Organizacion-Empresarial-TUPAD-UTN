bca28c19b03f60958ddf2ad45671b839cae3c057
M-y-M <magnello.marcos@gmail.com>
2026-06-16 18:27:16 -0300

    Actualización de estructura de main.py
    Re organice la estructura de main, incorporando ya las 3 instancias de este.
    1era: Pedido de DNI al usuario
    2da: menú con 3 opciones
    3era: menú, para solicitar nueva licencia.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from archivos import validar_dni
+from archivos import buscar_empleado
 #from solicitudes import generar_solicitud, modificar_tipo_licencia, modificar_fecha_tentativa
-from validaciones import buscar_empleado
+from validaciones import validar_dni
 
 
 
@@ -29,7 +29,6 @@ def procesar_mensaje(texto, estado):
 
 # BLOQUE PRINCIPAL
 def main():
-    while True:  # aca poner el menu con sus validaciones
         print("\n" + "=" * 50)
         print("SISTEMA DE GESTIÓN DE LICENCIAS".center(50))
         print("=" * 50)
@@ -37,67 +36,102 @@ def main():
             "\nBienvenido al ChatBot de solicitudes de livencia."
             "\nEscriba 'salir en cualquier momento para cancelar.\n"
         )
-        match estado_usuario["etapa"]:
-            case "iniciado":
-                entrada = input ("Por favor, inegrese su DNI (sin puntos ni espacios):  ").strip()
-                if entrada.lower() in ["salir","cancelar", "exit", "4"]:
-                    print("Proceso cancelado. ¡Hasta luego!")
-                    break
 
-                #Vamos con la primera validacion del DNI (Digitos)
-                dni_format_valido = validar_dni(entrada)
-                if dni_format_valido: 
-                    #vamos con la segunda validacion del DNI (Registros)
-                    empleado = buscar_empleado(dni_format_valido)
+#Bucle principal de la mqaquina de estados
+        while True:
+            match estado_usuario["etapa"]:
+                case "iniciado":
+                    #PEDIMOS EL DNI Y HACEMOS DOBLE VALIDACION
 
-                    if empleado: 
-                        estado_usuario["dni"] = dni_format_valido
-                        estado_usuario["nombre_empleado"] = empleado.get ("nombre", "Empleado")
-                        print(f"\n¡Hola, {estado_usuario['nombre_empleado']}! Autenticación existosa.")
+                    entrada = input ("Por favor, inegrese su DNI (sin puntos ni espacios):  ").strip()
+                    if entrada.lower() in ["salir","cancelar", "exit", "4"]:
+                        print("Proceso cancelado. ¡Hasta luego!")
+                        break
 
-                        estado_usuario ["estapa"] = "menu_tramite"
-                    
-                    else:#Si el formato es correcto pero no esta en el CSV
-                        print("El DNI ingresado no se encuentra registrado en el sistema."
-                            "\nPor favor, verifique el numero o cominiquese con RRHH.\n")
-                    
-                else:
-                    print("DNI INVÁLIDO. Ingrese un numero de 7 u 8 digitos sin puntos ni letras.\n")
-                    
-                    
-        while True:  # validacion de errores en las opciones del menu
-            try:
-                print("\n=============== MENÚ PRINCIPAL ===============")
-                print("\n¿Que desea hacer?"
-                    "\n1. Genera nueva solicitud"
-                    "\n2. Modificar tipo de licencia"
-                    "\n3. BModificar f3echa tentativa"
-                    "\n4. Salir.\n"
-                )
-                print("==============================================\n")
-                menu = int(input("Su eleccion: ")).strip()
-                print()
-                if menu not in range(1, 5):
-                    print(
-                        "No has ingresado un numero valido. Ingresa una opcion entre 1 y 4\n"
-                    )
-                else:
-                    break
-            except ValueError:
-                print(
-                    "No has ingresado una opcion valida. Ingresa solamente un numero entre 1 y 4.\n"
-                )
+                    #Vamos con la primera validacion del DNI (Digitos)
+                    dni_format_valido = validar_dni(entrada)
+                    if dni_format_valido: 
+                        #vamos con la segunda validacion del DNI (Registros)
+                        empleado = buscar_empleado(dni_format_valido)
 
-        # estructura match- case del menu principal
-        match menu:
-            case 1:
-                ...
+                        if empleado: 
+                            estado_usuario["dni"] = dni_format_valido
+                            estado_usuario["nombre_empleado"] = empleado.get ("nombre", "Empleado")
+                            print(f"\n¡Hola, {estado_usuario['nombre_empleado']}! Autenticación existosa.")
 
-            case 2:
-                ...
+                            estado_usuario ["estapa"] = "menu_tramite"
 
-            case 3:
-                ...
+                        else:#Si el formato es correcto pero no esta en el CSV
+                            print("El DNI ingresado no se encuentra registrado en el sistema."
+                                "\nPor favor, verifique el numero o cominiquese con RRHH.\n")
+
+                    else:
+                        print("DNI INVÁLIDO. Ingrese un numero de 7 u 8 digitos sin puntos ni letras.\n")
+
+            # --- ESTADO: MENÚ DE ELECCIÓN DE TRÁMITE ---
+            # Este bloque aparece inmediatamente después de que el DNI se validó con éxito
+                case "menu_tramite":
+                    #Menu pos-verificado del DNI
+
+                    print("\n============== ¿QUÉ DESEA REALIZAR? ==============")
+                    print("1. Iniciar una nueva solicitud de licencia")
+                    print("2. Consultar el estado de una licencia")
+                    print("3. Salir")
+                    print("==================================================")
+
+                    opcion_inicial = input("Su elección: ").strip()
+
+                    if opcion_inicial.lower() in ["salir", "cancelar", "exit", "3"]:
+                        print("Proceso finalizado. ¡Hasta luego!")
+                        return
+
+                    match opcion_inicial:
+                        case "1":
+                            print("\nPerfecto. Accediendo al sistema de solicitudes...")
+                            break
+
+                        case "2":
+                            print(f"\nConsultando el estado de la licencia para el DNI ingresado...")
+                            #Aca tenemos que llamar a la funcion de archivos.py
+                            #solicitud = buscar_solicitud(estado_usuario["dni"])
+                            #mostrar_estado(solicitud)
+
+                        case _:
+                            print("Opción no válida. Por favor, ingrese 1, 2 o 3.\n")
+
+                case "menu_principal":
+                    try:
+                        print("\n=============== MENÚ PRINCIPAL ===============")
+                        print("\n¿Que desea hacer?"
+                            "\n1. Genera nueva solicitud"
+                            "\n2. Modificar tipo de licencia"
+                            "\n3. BModificar f3echa tentativa"
+                            "\n4. Salir.\n"
+                        )
+                        print("==============================================\n")
+                        menu = int(input("Su eleccion: ")).strip()
+                        print()
+                        if menu not in range(1, 5):
+                            print(
+                                "No has ingresado un numero valido. Ingresa una opcion entre 1 y 4\n"
+                            )
+                        else:
+                            break
+                    except ValueError:
+                        print(
+                            "No has ingresado una opcion valida. Ingresa solamente un numero entre 1 y 4.\n"
+                        )
+
+                    # estructura match- case del menu principal
+                    match menu:
+                        case 1:
+                            ...
+
+                        case 2:
+                            ...
+
+                        case 3:
+                            ...
 
 
 if __name__ == "__main__":
