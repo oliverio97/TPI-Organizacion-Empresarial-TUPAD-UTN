e5e601992e73f5cf57174b632dee6f5baa60b415
M-y-M <magnello.marcos@gmail.com>
2026-06-16 17:33:55 -0300

    Definición función validar_dni
    Incorporo una segunda validación del DNI, verifica que sean dígitos numéricos y tenga una longitud lógica de 7 u 8 dígitos.

--- validaciones.py
+++ validaciones.py
@@ -1,7 +1,11 @@
 
-
-def validar_dni():
-    pass
+#Valida que el DNI sea numérico y tenga una longitud lógica (7 u 8 dígitos). 
+#Devuelve el DNI como entero si es válido, o None si no lo es.
+def validar_dni(dni_texto):
+    dni_texto = dni_texto.strip()
+    if dni_texto.isdigit() and 7 <= len(dni_texto) <=8:
+        return int(dni_texto)
+    return None
 
 def validar_fecha():
     pass
